a507cd85b0fdacdf0db3c9a9cc09ded0ac0b4b36
Burtorustum <aburur@d-e.org>
2017-08-09 20:25:58 -0400

    Split heuristic ai into two, one based on a pre-designated set of preferred plays and the other using a score system based on the state of the board. Completed the preffered play ai class.

---
+++ AiSrc/HeuristicEvaluation.java
@@ -0,0 +1,4 @@
+public class HeuristicEvaluation extends AiPlayer
+{
+
+}
---
+++ AiSrc/HeuristicPreferences.java
@@ -0,0 +1,23 @@
+public class HeuristicPreferences extends AiPlayer
+{
+    private int[][] prefferedMoves = {{1, 1}, {0, 0}, {0, 2}, {2, 0}, {2, 2}, {0, 1}, {1, 0}, {1, 2}, {2, 1}};
+
+    public HeuristicPreferences(Board board)
+    {
+        super(board);
+    }
+
+    public int[] move()
+    {
+        for (int[] move: prefferedMoves)
+        {
+            if (cells[move[0]][move[1]].getContent() == CellState.EMPTY)
+            {
+                return move;
+            }
+        }
+        assert false : "Bug! No empty cells???";
+        return null;
+    }
+
+}
